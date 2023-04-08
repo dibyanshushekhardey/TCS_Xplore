f1c06c3d2f80b3b2ddd425c07811178d5312b1af
Dibyanshu Shekhar Dey <66998557+dibyanshushekhardey@users.noreply.github.com>
2023-04-08 06:02:53 +0000

    added new files

---
+++ IPA_Practice_Questions/middleString.py
@@ -0,0 +1,16 @@
+def middleString(t, N):
+    #pass
+    middle=int(len(t)//2)
+    if len(t) % 2 != 0:
+        x=t[middle:middle+N]
+    else:
+        x=t[middle-1:middle+N]
+    if len(x) == 0:
+        return t[-1]
+    else:
+        return x
+
+
+t=input()
+N=int(input())
+print(middleString(t, N))
